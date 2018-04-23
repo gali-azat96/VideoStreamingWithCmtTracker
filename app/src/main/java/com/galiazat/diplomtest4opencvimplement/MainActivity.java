db0c03a4f509b3085fb7caf931acd2192fd6f3fd
Azat Galiullin <azat@warefly.com>
2018-04-23 14:22:23 +0300

    implementing find contours algorithm.

--- app/src/main/java/com/galiazat/diplomtest4opencvimplement/MainActivity.java
+++ app/src/main/java/com/galiazat/diplomtest4opencvimplement/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -13,8 +14,8 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -26,7 +27,6 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static org.opencv.core.CvType.CV_32SC1;
 import static org.opencv.core.CvType.CV_8U;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
@@ -41,10 +41,20 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @BindView(R.id.java_camera_view)
     JavaCameraView cameraView;
+    @BindView(R.id.seek_bar)
+    SeekBar seekBar;
+    @BindView(R.id.seekbar_value)
+    TextView textView;
 
-    Size size;
-    Mat markers;
-    List<Point> selectedPoints;
+    private Size size;
+    private UtilRectangle utilRectangle = new UtilRectangle();
+    private boolean isFinished = false;
+
+    double heightCoef;
+    double widthCoef;
+
+    double threshCoef = 0.5;
+    Mat oldMap;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -69,12 +79,28 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         ButterKnife.bind(this);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
         cameraView.setVisibility(View.VISIBLE);
         cameraView.setCvCameraViewListener(this);
+
+        seekBar.setProgress((int) (threshCoef*100));
+        textView.setText((int) (threshCoef*100) + "");
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                threshCoef = progress / 100.0;
+                textView.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         setOnTouchListener();
     }
@@ -83,45 +109,22 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         cameraView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Point point = new Point((int) event.getX(), (int) event.getY());
+                Point point = new Point((int) event.getX() * widthCoef,
+                        (int) event.getY() * heightCoef);
                 switch (event.getAction()){
                     case MotionEvent.ACTION_DOWN: {
-                        if (markers != null){
-                            markers.release();
-                        }
-                        markers = null;
-                        selectedPoints = new ArrayList<>();
-                        selectedPoints.add(point);
+                        isFinished = false;
+                        utilRectangle = new UtilRectangle();
+                        utilRectangle.addPoint(point);
                         return true;
                     }
                     case MotionEvent.ACTION_MOVE: {
-                        selectedPoints.add(point);
+                        utilRectangle.addPoint(point);
                         return true;
                     }
                     case MotionEvent.ACTION_UP: {
-                        selectedPoints.add(point);
-                        int viewHeight = cameraView.getHeight();
-                        int viewWidth = cameraView.getWidth();
-                        double matHeight = size.height;
-                        double matWidth = size.width;
-                        double heightCoef = matHeight / (viewHeight * 1.0);
-                        double widthCoef = matWidth / (viewWidth * 1.0);
-                        markers = new Mat(size, CV_32SC1, new Scalar(0));
-                        Scalar scalar = new Scalar(255, 255, 255, 0);
-                        Point lastPoint = selectedPoints.get(0);
-                        lastPoint.x *= widthCoef;
-                        lastPoint.y *= heightCoef;
-                        for (int i=1; i< selectedPoints.size(); i++){
-                            Point selectedPoint = selectedPoints.get(i);
-                            selectedPoint.x *= widthCoef;
-                            selectedPoint.y *= heightCoef;
-                            Imgproc.line(markers, lastPoint, selectedPoint, scalar);
-                            lastPoint = selectedPoint;
-                        }
-//                        markers.convertTo(markers, CV_8U);
-//                        Imgproc.cvtColor(markers, markers, Imgproc.COLOR_GRAY2BGR);
-//                        markers.convertTo(markers, CV_8UC1);
-//                        Imgproc.cvtColor(markers, markers, Imgproc.COLOR_GRAY2BGR);
+                        utilRectangle.addPoint(point);
+                        isFinished = utilRectangle.isAreaNotEqualsZero();
                         return true;
                     }
                 }
@@ -177,55 +180,73 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        if (oldMap != null){
+            oldMap.release();
+        }
+
         Mat mat = inputFrame.rgba();
         size = mat.size();
-//        if (mRgba != null) {
-//            mRgba.release();
-//        }
-//        mRgba = inputFrame.rgba();
-//        }
-//        Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_BGRA2BGR);
-//        mRgba.convertTo(mRgba, CV_8UC3);
-//        if (markers != null) {
-//            Imgproc.watershed(mRgba, markers);
-//        }
 
-//        Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
-        if (markers != null){
-            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGRA2BGR);
-            mat = steptowatershed(mat);
+        int viewHeight = cameraView.getHeight();
+        int viewWidth = cameraView.getWidth();
+        double matHeight = size.height;
+        double matWidth = size.width;
+        heightCoef = matHeight / (viewHeight * 1.0);
+        widthCoef = matWidth / (viewWidth * 1.0);
+
+        if (isFinished){
+            Mat newMat = new Mat(mat, utilRectangle.toOpenCvRect());
+            Imgproc.GaussianBlur(newMat, newMat, new Size(5, 5), 1, 1);
+            Imgproc.cvtColor(newMat, newMat, Imgproc.COLOR_BGRA2GRAY);
+            Imgproc.threshold(newMat, newMat, threshCoef * 255,
+                    255, Imgproc.THRESH_BINARY);
+            List<MatOfPoint> matOfPointList = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(newMat, matOfPointList, hierarchy, Imgproc.RETR_EXTERNAL,
+                    Imgproc.RETR_LIST, new Point(utilRectangle.getLeft(), utilRectangle.getTop()));
+            hierarchy.release();
+            Imgproc.drawContours(mat, matOfPointList, -1, new Scalar(234,18,94));
+            newMat.convertTo(newMat, CV_8U);
+            Imgproc.cvtColor(newMat, newMat, Imgproc.COLOR_GRAY2RGB);
+            Imgproc.resize(newMat, newMat, size);
+            newMat.release();
+//            mat.release();
+//            oldMap = newMat;
+            return mat;
+//            mat = newMat;
         }
 //        steptowatershed(mRgba);
         return mat;
 
     }
 
-    public Mat steptowatershed(Mat img)
-    {
-        WatershedSegmenter segmenter = new WatershedSegmenter();
-        segmenter.setMarkers(markers);
-        return segmenter.process(img);
-    }
-    public class WatershedSegmenter
-    {
-        public Mat mMarkers;
-
-        public void setMarkers(Mat markerImage)
-        {
-            mMarkers = markerImage.clone();
-        }
-
-        public Mat process(Mat image)
-        {
-            Imgproc.watershed(image, mMarkers);
-            mMarkers.convertTo(mMarkers, CV_8U);
-            Imgproc.cvtColor(mMarkers, mMarkers, Imgproc.COLOR_GRAY2BGR);
-            Core.add(image, mMarkers, image);
-            mMarkers.release();
-//            image.release();
+//    public Mat steptowatershed(Mat img)
+//    {
+//        WatershedSegmenter segmenter = new WatershedSegmenter();
+//        segmenter.setMarkers(markers);
+//        return segmenter.process(img);
+//    }
+//    public class WatershedSegmenter
+//    {
+//        public Mat mMarkers;
+//
+//        public void setMarkers(Mat markerImage)
+//        {
+//            mMarkers = markerImage.clone();
+//        }
+//
+//        public Mat process(Mat image)
+//        {
+//            Imgproc.watershed(image, mMarkers);
 //            mMarkers.convertTo(mMarkers, CV_8U);
 //            Imgproc.cvtColor(mMarkers, mMarkers, Imgproc.COLOR_GRAY2BGR);
-            return image;
-        }
-    }
+//            Core.add(image, mMarkers, image);
+//            mMarkers.release();
+////            image.release();
+////            mMarkers.convertTo(mMarkers, CV_8U);
+////            Imgproc.cvtColor(mMarkers, mMarkers, Imgproc.COLOR_GRAY2BGR);
+//            return image;
+//        }
+//    }
 }
